dc722f204b5f90632e6a152d58222e3de2e4213d
niklus14 <ssenkrad14@gmail.com>
2025-11-21 09:12:01 +0000

    Data Structures: Lists, Tuples

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list):
-    n=-len(my_list)
-    m=-1
-    while n<=m:
+    n = -len(my_list)
+    m = -1
+    while n <= m:
         print("{:d}".format(my_list[m]))
-        m-=1
+        m -= 1
