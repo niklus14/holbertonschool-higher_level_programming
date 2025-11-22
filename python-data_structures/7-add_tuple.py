87c2095a154cf0e368076d22daa57d033b083bbc
niklus14 <ssenkrad14@gmail.com>
2025-11-22 22:49:59 +0000

    Data Structures: Lists, Tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,16 +1,4 @@
-#!/usr/bin/python3
-def add_tuple(tuple_a, tuple_b):
-    if len(tuple_a) == 0:
-        tuple_a = (0, 0)
-    elif len(tuple_a) == 1:
-        tuple_a = (tuple_a[0], 0)
-    else:
-        tuple_a = (tuple_a[0], tuple_a[1])
-    if len(tuple_b) == 0:
-        tuple_b = (0, 0)
-    elif len(tuple_b) == 1:
-        tuple_b = (tuple_b[0], 0)
-    else:
-        tuple_b = (tuple_b[0], tuple_b[1])
-    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return new_tuple
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])
