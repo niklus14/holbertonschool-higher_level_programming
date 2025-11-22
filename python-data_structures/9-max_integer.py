586d436df2e7614cc976716856989842d3a27e4e
niklus14 <ssenkrad14@gmail.com>
2025-11-22 23:21:44 +0000

    Data Structures: Lists, Tuples

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,9 +2,9 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None    
-    for k in range(my_list):
-        for i in range(my_list):
-            hm = 0:
+    for k in (my_list):
+        hm = 0
+        for i in (my_list):
             if k >= i:
                 hm +=1
         if hm == len(my_list):
