97fadbb907c31ae1ea0acb9f03bd94b1515b6c17
niklus14 <ssenkrad14@gmail.com>
2025-11-22 21:52:46 +0000

    Data Structures: Lists, Tuples

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    my_string = list(my_string)
     a = 0
-    my_string=list(my_string)
-    for i in (my_string):
-        if i == "c" or i == "C" :
-            del my_string[a]    
+    while a < len(my_string):
+        if my_string[a] == "c" or my_string[a] == "C":
+            del my_string[a]
         else:
-            a +=1
-    return "".join(my_string)   
+            a += 1
+    return "".join(my_string)
