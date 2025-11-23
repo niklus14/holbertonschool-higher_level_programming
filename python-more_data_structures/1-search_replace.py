f7c4a713b6ca3bf48eae835c3551f4e8bd3b2b5b
niklus14 <ssenkrad14@gmail.com>
2025-11-23 00:17:38 +0000

    Data Structures: Lists, Tuples

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new=[]
+    for k in my_list:
+        if k == search:
+            new.append(replace)
+        else:
+            new.append(k)
+    return new
