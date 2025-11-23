c252782a0fc795a16d3cda3055d301f655d363e9
niklus14 <ssenkrad14@gmail.com>
2025-11-23 00:49:19 +0000

    Data Structures: Lists, Tuples

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def uniq_add(my_list):
-    x = 0 
-    new = [0]
+    total = 0
+    new = []
+
     for k in my_list:
-        for i in new:
-            if k != i:
-                new.append(k)
+        if k not in new:
+            new.append(k)
+
     for y in new:
-        x = x + y
-    return x
+        total += y
+
+    return total
