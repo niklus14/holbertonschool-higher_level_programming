33e2f5522e7d69bc4e41048fc98f1b923760cf96
niklus14 <ssenkrad14@gmail.com>
2025-12-02 20:46:21 +0000

    Have a fun bud with these pathetic codes

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Uses the GitHub API to display your user id"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]  # Personal access token used as password
+
+    url = "https://api.github.com/user"
+
+    # Basic Authentication
+    response = requests.get(url, auth=(username, token))
+
+    try:
+        data = response.json()
+        print(data.get("id"))
+    except ValueError:
+        print("None"")
