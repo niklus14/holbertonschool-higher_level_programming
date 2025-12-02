4ffae6e4d5e675b43185f119fa664eff2f53ad42
niklus14 <ssenkrad14@gmail.com>
2025-12-02 19:59:42 +0000

    Look at

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Bro still have some understanding problems just look at "again"""
+
 import urllib.request
+
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     a = urllib.request.Request(url, headers={'cfclearance': 'true'})
+
     with urllib.request.urlopen(url) as f:
         s = f.read()
+
     print("Body response:")
     print("\t- type: {}".format(type(s)))
     print("\t- content: {}".format(s))
